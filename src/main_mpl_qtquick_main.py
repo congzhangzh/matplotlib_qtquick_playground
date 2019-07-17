e4e9d2b5867ae8544bc96ada9233e23c83cb4d5b
Cong Zhang <congzhangzh@gmail.com>
2019-07-17 16:45:45 +0800

    the final target work in progress

--- src/main_mpl_qtquick_main.py
+++ src/main_mpl_qtquick_main.py
@@ -27,6 +27,7 @@ from backend.backend_qquick5agg import FigureCanvasQTAgg
 faulthandler.enable()
 
 import PySide2
+from PySide2 import QtCore
 from PySide2.QtCore import QAbstractListModel, QModelIndex, QObject, QSize, Qt, QUrl
 from PySide2.QtCore import QObject, Signal, Slot, Property
 from PySide2.QtGui import QGuiApplication, QColor, QImage, QPixmap
@@ -77,16 +78,16 @@ class DataSeriesModel(QAbstractListModel):
         
         self._data_series = list()
         self._length_data = 0
-    
-    @pyqtProperty(int, notify=lengthDataChanged)
-    def lengthData(self):
+
+    def get_lengthData(self):
         return self._length_data
     
-    @lengthData.setter
-    def lengthData(self, length):
+    def set_lengthData(self, length):
         if self._length_data != length:
             self._length_data = length
             self.lengthDataChanged.emit()
+
+    lengthData=QtCore.Property(int, get_lengthData, set_lengthData, notify=lengthDataChanged)
 
     def roleNames(self):
         return self._roles
@@ -164,12 +165,10 @@ class Form(QObject):
         
         self._data = data
 
-    @property
-    def figure(self):
+    def get_figure(self):
         return self._figure
-    
-    @figure.setter
-    def figure(self, fig):
+
+    def set_figure(self, fig):
         self._figure = fig
         self._figure.set_facecolor('white')
         self.axes = self.figure.add_subplot(111)    
@@ -179,23 +178,23 @@ class Form(QObject):
         self.xToChanged.connect(self._figure.canvas.draw_idle)
         self.legendChanged.connect(self._figure.canvas.draw_idle)
         self.stateChanged.connect(self._figure.canvas.draw_idle)
-        
-    @pyqtProperty('QString', notify=statusTextChanged)
-    def statusText(self):
+
+    figure=QtCore.Property(QtCore.QObject, get_figure, set_figure)
+
+    def get_statusText(self):
         return self._status_text
-    
-    @statusText.setter
-    def statusText(self, text):
+
+    def set_statusText(self, text):
         if self._status_text != text:
             self._status_text = text
             self.statusTextChanged.emit()
 
-    @pyqtProperty('QString')
-    def filename(self):
+    statusText=QtCore.Property(str, get_statusText, set_statusText, notify=statusTextChanged)
+
+    def get_filename(self):
         return self._filename
-    
-    @filename.setter
-    def filename(self, filename):
+
+    def set_filename(self, filename):
         if filename:
             filename = QUrl(filename).toLocalFile()
             if filename != self._filename:
@@ -205,12 +204,13 @@ class Form(QObject):
                 self.xTo = self._data.lengthData
                 self.update_figure()
 
-    @pyqtProperty(int, notify=xFromChanged)
-    def xFrom(self):
+    filename=QtCore.Property(str, get_filename, set_filename)
+
+
+    def get_xFrom(self):
         return self._x_from
-    
-    @xFrom.setter
-    def xFrom(self, x_from):
+
+    def set_xFrom(self, x_from):
         if self.figure is None:
             return
             
@@ -219,13 +219,13 @@ class Form(QObject):
             self._x_from = x_from
             self.axes.set_xlim(left=self._x_from)
             self.xFromChanged.emit()
-        
-    @pyqtProperty(int, notify=xToChanged)
-    def xTo(self):
+
+    xFrom=Property(int, get_xFrom, set_xFrom, notify=xFromChanged)
+
+    def get_xTo(self):
         return self._x_to
-    
-    @xTo.setter
-    def xTo(self, x_to):
+
+    def set_xTo(self, x_to):
         if self.figure is None:
             return
             
@@ -234,13 +234,13 @@ class Form(QObject):
             self._x_to = x_to
             self.axes.set_xlim(right=self._x_to)
             self.xToChanged.emit()
-        
-    @pyqtProperty(bool, notify=legendChanged)
-    def legend(self):
+
+    xTo=Property(int, get_xTo, set_xTo, notify=xToChanged)
+
+    def get_legend(self):
         return self._legend
-    
-    @legend.setter
-    def legend(self, legend):
+
+    def set_legend(self, legend):
         if self.figure is None:
             return
             
@@ -253,14 +253,16 @@ class Form(QObject):
                 if leg is not None:
                     leg.remove()
             self.legendChanged.emit()
-    
-    @pyqtProperty(str, constant=True)
-    def about(self):
+
+    legend=QtCore.Property(bool, get_legend, set_legend, notify=legendChanged)
+
+    def get_about(self):
         msg = __doc__
         return msg.strip()
-        
+
+    about=QtCore.Property(str, get_about)
     
-    @pyqtSlot()
+    @QtCore.Slot()
     def update_figure(self):
         if self.figure is None:
             return
